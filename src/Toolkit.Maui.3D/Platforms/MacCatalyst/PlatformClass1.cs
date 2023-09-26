dc366599002ca0b2d62123d405094b35fd46e9e9
Stephen Quan <squan@esri.com>
2023-09-26 13:23:32 +1000

    Rename Toolkit.Maui.Shapes3D.Sample

--- src/Toolkit.Maui.3D/Platforms/MacCatalyst/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Toolkit.Maui._3D
-{
-    // All the code in this file is only included on Mac Catalyst.
-    public class PlatformClass1
-    {
-    }
-}
--- src/Toolkit.Maui.3D/Platforms/Windows/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Toolkit.Maui._3D
-{
-    // All the code in this file is only included on Windows.
-    public class PlatformClass1
-    {
-    }
-}
